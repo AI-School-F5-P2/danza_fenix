66a48485a58fe47e242438cb22080f0dd0f07bdd
unknown <montenegrotello@gmail.com>
2023-07-21 14:12:46 +0200

    se modificaron y arreglaron los errores en tabla alumno

--- apis/alumnos.py
+++ apis/alumnos.py
@@ -6,29 +6,39 @@ router = APIRouter()
 
 @router.get("/mostrar_alumnos", tags = ["alumnos"])
 def mostrar_alumnos():
+    """<h1>Metodo para mostrar alumnos. </h1>
+    <p>Este metodo devuelve todos los alumnos<p/>"""
     return qw_get_alumnos()
 
 
 @router.get("/mostrar_alumno", tags = ["alumnos"])
 def mostrar_alumno(dni_alumno: str):
+    """<h1>Metodo para buscar un anico alumno.</h1>
+    <p> Este metodo devuelve el usuario del alumno buscando. </p>"""
     return qw_get_alumno(dni_alumno)
 
 
 
 @router.post("/insertar", tags = ["alumnos"])
 def insertar_alumnos(alumno: AlumnoValidator):
+    """<h1>Metodo para insertar alumnos. <h1/>
+    <p>Este metodo permite insertar alumnos<p/> """
     return qw_post_alumnos(alumno.dict())
 
 
 
 @router.put("/actualizar", tags = ["alumnos"])
 def actualizar_alumnos(dni_alumno:str,alumno: AlumnoValidator):
+    """<h1>Metodo para actualiza alumnos<h1/>
+    <p>Este metodo permite modificar los datos del alumno<p/>"""
     return qw_put_alumnos(dni_alumno, alumno.dict())
 
 
 
 @router.delete("/eliminar", tags = ["alumnos"])
 def eliminar_alumno(dni_eliminar_alumno:str):
+    """Metodo para eliminar alumnos.<h1/>
+    <p> Este metodo permite eliminar alumnos<p/>"""
     return qw_delete_alumno(dni_eliminar_alumno)
 
 
